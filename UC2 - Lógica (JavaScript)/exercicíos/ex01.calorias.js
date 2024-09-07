d4f81c06c21a44cde0ce02cac42005de3540af36
Joaquimmh <matheusjoaquim15@gmail.com>
2024-09-07 13:24:06 -0300

    ...

--- UC2 - Lógica (JavaScript)/exercicíos/ex01.calorias.js
+++
@@ -1,9 +0,0 @@
-function calcularCalorias(){
-    let prato = parseInt(document.getElementById("prato").value);
-    let sobremesa = parseInt(document.getElementById("sobremesa").value);
-    let bebida = parseInt(document.getElementById("bebida").value);
-
-    let totalCalorias = prato + sobremesa + bebida;
-
-    document.getElementById("resultudo").innerText = "Total de calorias " + totalCalorias + "cal";
-}
